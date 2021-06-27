dccc522f4b32236857d23f4f425bd0f92be22161
Lucemario Felix <jlucemariof@gmail.com>
2021-06-27 09:43:40 -0300

    update perguntas

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -51,7 +51,7 @@ export function Room() {
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
 
-    roomRef.once("value", (room) => {
+    roomRef.on("value", (room) => {
       const databaseRoom = room.val();
       const firebaseQuestion: FirebaseQuestion = databaseRoom.questions ?? {};
 
